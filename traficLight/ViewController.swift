44ee01636f23a6c4f64a090304a204893a70276b
calculator <ddd>
2022-01-09 11:44:01 +0600

    Initial Commit

---
+++ traficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  traficLight
+//
+//  Created by Икрамджан Абдукадыров on 9/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
