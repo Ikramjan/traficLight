ed192cabd53d8b370e9d072869247929c5e5fb62
calculator <ddd>
2022-01-09 16:34:39 +0600

    done home work traffic light

--- traficLight/ViewController.swift
+++ traficLight/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var traficLight: UIView!
+    @IBOutlet var traficLightTwo: UIView!
+    @IBOutlet var traficlightThree: UIView!
+    @IBOutlet var nextButtonTrafic: UIButton!
+    
+    let currentAlpha = 0.3
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        traficLight.layer.cornerRadius = 100
+        traficLightTwo.layer.cornerRadius = 100
+        traficlightThree.layer.cornerRadius = 100
+        nextButtonTrafic.layer.cornerRadius = 10
+        traficLight.alpha = currentAlpha
+        traficLightTwo.alpha = currentAlpha
+        traficlightThree.alpha = currentAlpha
     }
 
 
+    @IBAction func nextButtontChange() {
+        nextButtonTrafic.setTitle("NEXT", for: .normal)
+        
+        if traficLight.alpha == traficLightTwo.alpha {
+            traficLight.alpha = 1
+            traficlightThree.alpha = currentAlpha
+        } else if traficLight.alpha > currentAlpha && traficLightTwo.alpha == traficlightThree.alpha {
+            traficLightTwo.alpha = 1
+            traficLight.alpha = currentAlpha
+        } else if traficLightTwo.alpha > currentAlpha && traficlightThree.alpha == traficLight.alpha {
+            traficlightThree.alpha = 1
+            traficLightTwo.alpha = currentAlpha
+        }
+        
+    }
 }
 
